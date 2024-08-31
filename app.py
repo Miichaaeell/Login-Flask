b9935a39208695be725cfc03132669dbbd14215f
Michael Andrew <96355111+Miichaaeell@users.noreply.github.com>
2024-08-30 22:36:14 -0300

    inicio de cada tela
    projeto inicial

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Index Hello World"
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/cadastrado', methods=['POST'])
+def cadastrado():
+    nome = request.form.get('user')
+    return render_template('cadastrado.html', nome = nome)
+@app.route('/painel', methods=['POST'])
+def meu_painel():
+    nome = request.form.get('user')
+    return render_template('painel.html', nome = nome)
 
 if __name__ == '__main__':
     app.run(debug=True)
